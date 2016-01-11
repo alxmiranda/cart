f59acbf3b3b208f26804779021e1b1af78f9032b
Alex <amdesignercontato@gmail.com>
2016-01-11 16:11:03 -0200

    Recuperando itens clicados e add no localstorage

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
 var cart = (function(){
 
+	var btn = document.querySelectorAll("button");
+	var arraItens = [];
+
+	function addItem(index){
+
+		function clickItem(){
+			// recuperando dados do item
+			var getItem   = btn[index].parentNode.parentNode,
+							id        = index + 1,
+							name      = getItem.querySelector(".product-name").innerText,
+							price     = getItem.querySelector(".product-price").innerText,
+							qty       = 0;
+
+			// convertendo array em string
+			var item   = 
+							{
+								id : id,
+								name : name,
+								price : price,
+								qty : qty
+							};
+			arraItens.push(item);
+
+			// salvando string no localstorage
+			localStorage.setItem("cart", JSON.stringify(arraItens));
+
+		}
+
+		btn[index].addEventListener("click", clickItem, true);			
+	}
+
+	for(var i = 0; i < btn.length; i++){
+		addItem(i);
+	}
 }())
