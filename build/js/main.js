b8b97fe7a1de5e984d719799c8b6d5ba75ca4cd6
Alex <amdesignercontato@gmail.com>
2017-03-01 01:25:56 -0300

    Ajustes no layout e aplicação do script para add ao carrinho.

--- build/js/main.js
+++ build/js/main.js
@@ -1,7 +1,41 @@
 "use strict";
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+var BudgetCart = function BudgetCart() {
+  'use strict';
 
-[1, 2, 3, 4, undefined, true, false].forEach(function (e) {
-    console.log(typeof e === "undefined" ? "undefined" : _typeof(e));
-});
+  var _ = this;
+
+  _.config = {
+    cartStorage: JSON.parse(localStorage.getItem("BudgetCart")),
+    btnItem: Array.prototype.slice.call(document.querySelectorAll(".product__actions .btn")),
+    cartTotal: document.querySelector(".header__notifications p span")
+  };
+
+  _.nItens = function () {
+    _.config.cartTotal.innerText = _.config.cartStorage.length;
+  };
+
+  _.saveItem = function (el, i) {
+
+    var itemDetails = {};
+
+    itemDetails.id = i;
+    itemDetails.name = el.parentNode.parentNode.querySelector(".product__name").innerText;
+    itemDetails.price = el.parentNode.parentNode.querySelector(".product__price").innerText;
+
+    _.config.cartStorage.push(itemDetails);
+    localStorage.setItem("BudgetCart", JSON.stringify(_.config.cartStorage));
+    _.nItens();
+  };
+
+  _.config.cartStorage === null ? _.config.cartStorage = [] : "";
+
+  _.nItens();
+
+  _.config.btnItem.forEach(function (el, i, arr) {
+    el.addEventListener("click", function () {
+      _.saveItem(el, i);
+    });
+  });
+};
+new BudgetCart();
