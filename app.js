57e504d3b054a6373f4c98c090bab45db7ea62cc
Alex Miranda <amdesignercontato@gmail.com>
2017-07-09 02:13:21 -0300

    Atualizando as rotas

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const app = express();
 
 app.use("/build", express.static(__dirname + "/build"))
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/build/index.html')
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/build/pages/home/index.html')
+})
+
+app.get('/checkout', (req, res) => {
+  res.sendFile(__dirname + '/build/pages/checkout/index.html')
 })
 
 app.listen(3000, (req, res) => console.log("Aplicação rodando na porta 3000"))
